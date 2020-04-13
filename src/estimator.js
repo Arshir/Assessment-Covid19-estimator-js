1658ee5c8cdf7f31b59cc4dcb1e88acab8e118dd
Kani <send2fareed@gmail.com>
2020-04-13 14:19:30 +0000

    Removed test data

--- src/estimator.js
+++ src/estimator.js
@@ -125,18 +125,4 @@ const covid19ImpactEstimator = (data) => {
   return output;
 };
 
-const data = {
-  region: {
-    name: 'Africa',
-    avgAge: 19.7,
-    avgDailyIncomeInUSD: 4,
-    avgDailyIncomePopulation: 0.73
-  },
-  periodType: 'days',
-  timeToElapse: 38,
-  reportedCases: 2747,
-  population: 92931687,
-  totalHospitalBeds: 678874
-};
-
-console.log(covid19ImpactEstimator(data));
+export default covid19ImpactEstimator;
